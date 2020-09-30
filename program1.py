762c2f7e1e06dac8cdde111e7e87fabd64e6ddc3
Karthik <53967591+Karthikdinesh208@users.noreply.github.com>
2020-09-30 12:15:41 -0700

    program

---
+++ program1.py
@@ -0,0 +1,15 @@
+str=input("Enter a number")
+str1=input("Enter the second number")
+a=int(str)
+b=int(str1)
+
+if(b!=0):
+    sum=a+b
+    print(sum)
+    diff=a-b
+    print(diff)
+    mul=a*b
+    print(mul)
+    div=a/b
+    print(div)
+print(Successfully executed)
